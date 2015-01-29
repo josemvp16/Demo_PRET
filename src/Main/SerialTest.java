8d635f6078ee3962c54cc30d05837ca89658801d
SWO <SWO@SWO-.lan>
2015-01-29 11:00:09 -0600

    new commit

--- src/Main/SerialTest.java
+++ src/Main/SerialTest.java
@@ -1,124 +1,145 @@
 package Main;
 
-import Database.ConsultaVotos;
+import Database.DBQueries;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import gnu.io.CommPortIdentifier; 
+import gnu.io.CommPortIdentifier;
 import gnu.io.SerialPort;
-import gnu.io.SerialPortEvent; 
-import gnu.io.SerialPortEventListener; 
+import gnu.io.SerialPortEvent;
+import gnu.io.SerialPortEventListener;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
 import java.util.Enumeration;
 
 public class SerialTest implements SerialPortEventListener {
-SerialPort serialPort;
-/** The port we're normally going to use. */
-private static final String PORT_NAMES[] = { 
-"/dev/tty.usbserial-A9007UX1", // Mac OS X
-"/dev/ttyACM0", // Raspberry Pi
-"/dev/ttyUSB0", // Linux
-"COM4", // Windows
-};
-/**
-* A BufferedReader which will be fed by a InputStreamReader 
-* converting the bytes into characters 
-* making the displayed results codepage independent
-*/
-private BufferedReader input;
-/** The output stream to the port */
-private OutputStream output;
-/** Milliseconds to block while waiting for port open */
-private static final int TIME_OUT = 8000;
-/** Default bits per second for COM port. */
-private static final int DATA_RATE = 19200;
-public void initialize() {
+
+    SerialPort serialPort;
+    String remitente;
+    String mensaje;
+    /**
+     * The port we're normally going to use.
+     */
+    private static final String PORT_NAMES[] = {
+        "/dev/tty.usbserial-A9007UX1", // Mac OS X
+        "/dev/ttyACM0", // Raspberry Pi
+        "/dev/ttyUSB0", // Linux
+        "COM3", // Windows
+    };
+    /**
+     * A BufferedReader which will be fed by a InputStreamReader converting the
+     * bytes into characters making the displayed results codepage independent
+     */
+    private BufferedReader input;
+    /**
+     * The output stream to the port
+     */
+    //private OutputStream output;
+    private BufferedWriter output;
+    /**
+     * Milliseconds to block while waiting for port open
+     */
+    private static final int TIME_OUT = 8000;
+    /**
+     * Default bits per second for COM port.
+     */
+    private static final int DATA_RATE = 19200;
+
+    public void initialize() {
 // the next line is for Raspberry Pi and 
 // gets us into the while loop and was suggested here was suggested http://www.raspberrypi.org/phpBB3/viewtopic.php?f=81&t=32186
 //System.setProperty("gnu.io.rxtx.SerialPorts", "/dev/ttyACM0");
-CommPortIdentifier portId = null;
-Enumeration portEnum = CommPortIdentifier.getPortIdentifiers();
+        CommPortIdentifier portId = null;
+        Enumeration portEnum = CommPortIdentifier.getPortIdentifiers();
 //First, Find an instance of serial port as set in PORT_NAMES.
-while (portEnum.hasMoreElements()) {
-CommPortIdentifier currPortId = (CommPortIdentifier) portEnum.nextElement();
-for (String portName : PORT_NAMES) {
-if (currPortId.getName().equals(portName)) {
-portId = currPortId;
-break;
-}
-}
-}
-if (portId == null) {
-System.out.println("Could not find COM port.");
-return;
-}
-try {
+        while (portEnum.hasMoreElements()) {
+            CommPortIdentifier currPortId = (CommPortIdentifier) portEnum.nextElement();
+            for (String portName : PORT_NAMES) {
+                if (currPortId.getName().equals(portName)) {
+                    portId = currPortId;
+                    break;
+                }
+            }
+        }
+        if (portId == null) {
+            System.out.println("Could not find COM port.");
+            return;
+        }
+        try {
 // open serial port, and use class name for the appName.
-serialPort = (SerialPort) portId.open(this.getClass().getName(),
-TIME_OUT);
+            serialPort = (SerialPort) portId.open(this.getClass().getName(),
+                    TIME_OUT);
 // set port parameters
-serialPort.setSerialPortParams(DATA_RATE,
-SerialPort.DATABITS_8,
-SerialPort.STOPBITS_1,
-SerialPort.PARITY_NONE);
+            serialPort.setSerialPortParams(DATA_RATE,
+                    SerialPort.DATABITS_8,
+                    SerialPort.STOPBITS_1,
+                    SerialPort.PARITY_NONE);
 // open the streams
-input = new BufferedReader(new InputStreamReader(serialPort.getInputStream()));
-output = serialPort.getOutputStream();
+            input = new BufferedReader(new InputStreamReader(serialPort.getInputStream()));
+            //output = serialPort.getOutputStream();
+            output = new BufferedWriter(new OutputStreamWriter(serialPort.getOutputStream()));
 // add event listeners
-serialPort.addEventListener(this);
-serialPort.notifyOnDataAvailable(true);
-} catch (Exception e) {
+            serialPort.addEventListener(this);
+            serialPort.notifyOnDataAvailable(true);
+        } catch (Exception e) {
 //System.err.println(e.toString());
-}
-}
-/**
-* This should be called when you stop using the port.
-* This will prevent port locking on platforms like Linux.
-*/
-public synchronized void close() {
-if (serialPort != null) {
-serialPort.removeEventListener();
-serialPort.close();
-}
-}
-/**
-* Handle an event on the serial port. Read the data and print it.
-*/
-public synchronized void serialEvent(SerialPortEvent oEvent) {
-if (oEvent.getEventType() == SerialPortEvent.DATA_AVAILABLE) {
-try {
-String inputLine=input.readLine();
-System.out.println(inputLine);
-String res[] = inputLine.split(";");
-if (res.length == 3){
-System.out.println(res[0]);
-System.out.println(res[1]);
-System.out.println(res[2]);
-//System.out.println(inputLine);
+        }
+    }
 
-ConsultaVotos cv= new ConsultaVotos();
-cv.updateEstado(res[0], res[1], res[2]);
-}else{
+    /**
+     * This should be called when you stop using the port. This will prevent
+     * port locking on platforms like Linux.
+     */
+    public synchronized void close() {
+        if (serialPort != null) {
+            serialPort.removeEventListener();
+            serialPort.close();
+        }
+    }
 
-        ConsultaVotos cv= new ConsultaVotos();
-        cv.updateVotos(res[0], Integer.parseInt(res[1]), Integer.parseInt(res[2]), Integer.parseInt(res[3]));
-}    
-} catch (Exception e) {
-//System.err.println(e.toString());
-}
-}
-// Ignore all the other eventTypes, but you should consider the other ones.
-}
-public static void main(String[] args) throws Exception {
-SerialTest main = new SerialTest();
-main.initialize();
-Thread t=new Thread() {
-public void run() {
+    /**
+     * Handle an event on the serial port. Read the data and print it.
+     */
+    public synchronized void serialEvent(SerialPortEvent oEvent) {
+        if (oEvent.getEventType() == SerialPortEvent.DATA_AVAILABLE) {
+            try {
+                String inputLine = input.readLine();
+                //System.out.println(inputLine);
+                if (inputLine.equals("")) {
+                    //System.out.println("no trae nada");
+                } else if (inputLine.contains("+52")) {
+                    //System.out.println("estos son los datos de emisor: " + inputLine);
+                    remitente = inputLine;
+                } else if (!inputLine.contains("\"")) {
+                    //System.out.println("este es el mensaje: " + inputLine);
+                    mensaje = inputLine;
+                    String textoEntrada = remitente + "\n" + mensaje;
+                    System.out.println("Llama al metodo con estos datos:  \n" + textoEntrada);
+                    Identification id = new Identification();
+                    id.prepareText(textoEntrada);
+                }
+            } catch (Exception e) {
+                System.err.println("Error " + e.getMessage());
+            }//END TRY/CATCH
+        }//END IF
+        // Ignore all the other eventTypes, but you should consider the other ones.
+    }//END METHOD
+
+    public static void main(String[] args) throws Exception {
+        SerialTest main = new SerialTest();
+        main.initialize();
+
+        Thread t = new Thread() {
+            public void run() {
 //the following line will keep this app alive for 1000 seconds,
 //waiting for events to occur and responding to them (printing incoming messages to console).
-try {Thread.sleep(2000);} catch (InterruptedException ie) {}
-}
-};
-t.start();
-System.out.println("Started");
-}
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException ie) {
+                }
+            }
+        };
+        t.start();
+        System.out.println("Started");
+    }
 }
